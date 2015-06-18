f0597b3a9d822e6d00d98b7808001aacf0d069b3
Workings <cpdavies@gmail.com>
2015-06-18 11:44:22 +0100

    Adding data export to csv

--- www/js/modules/settings/settings-controller.js
+++ www/js/modules/settings/settings-controller.js
@@ -44,7 +44,7 @@ function($scope,$rootScope,$ionicModal,$state,ProfileService,UserService,LoginSe
     $scope.loadConnections = function() {
         AnalyticsService.event('Settings Screen', {'Action':'Load Connections'});
 
-        window.open('https://platform.ginsberg.io/account/myconnections', '_system');
+        window.open('https://splatform.ginsberg.io/account/myconnections', '_system');
     };
 
 
@@ -163,6 +163,194 @@ function($scope,$rootScope,$ionicModal,$state,ProfileService,UserService,LoginSe
       };
       
       
+       
+      // private property
+        var _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+        
+      // private method for UTF-8 encoding
+        var _utf8_encode = function (string) {
+            string = string.replace(/\r\n/g,"\n");
+            var utftext = "";
+
+            for (var n = 0; n < string.length; n++) {
+
+                var c = string.charCodeAt(n);
+
+                if (c < 128) {
+                    utftext += String.fromCharCode(c);
+                }
+                else if((c > 127) && (c < 2048)) {
+                    utftext += String.fromCharCode((c >> 6) | 192);
+                    utftext += String.fromCharCode((c & 63) | 128);
+                }
+                else {
+                    utftext += String.fromCharCode((c >> 12) | 224);
+                    utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                    utftext += String.fromCharCode((c & 63) | 128);
+                }
+
+            }
+
+            return utftext;
+        };
+        
+      // public method for encoding
+        var encode = function (input) {
+            var output = "";
+            var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+            var i = 0;
+
+            input = _utf8_encode(input);
+
+            while (i < input.length) {
+
+                chr1 = input.charCodeAt(i++);
+                chr2 = input.charCodeAt(i++);
+                chr3 = input.charCodeAt(i++);
+
+                enc1 = chr1 >> 2;
+                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                enc4 = chr3 & 63;
+
+                if (isNaN(chr2)) {
+                    enc3 = enc4 = 64;
+                } else if (isNaN(chr3)) {
+                    enc4 = 64;
+                }
+
+                output = output +
+                _keyStr.charAt(enc1) + _keyStr.charAt(enc2) +
+                _keyStr.charAt(enc3) + _keyStr.charAt(enc4);
+
+            }
+
+            return output;
+        };
+
+        var tempString = "";
+        var TotalColumns = 30;
+        
+        var finishTempLine = function()
+        {
+            var lines = tempString.split('\n');
+            var current = (lines[lines.length-1].length - lines[lines.length-1].replace(new RegExp(',',"g"), '').length);
+            var toGo = TotalColumns - current;
+            
+            var addition = "";
+            for(var i = 0; i < toGo; ++i)
+            {
+                addition += ',';
+            }
+            
+            tempString += addition;
+        };
+        
+        var checkFor = function(key, type, start)
+        {
+            var i = key.indexOf(type);
+            if(i === 0 || i === 1)
+            {
+                var j = key.indexOf(type + '._');
+                if(j !== i)
+                {
+                    var code = JSON.parse(localStorage.getItem(key));
+                    //Extract fields
+                    if(start === false)
+                    {
+                        tempString = "\n";
+                        finishTempLine();
+                        tempString += "\n";
+                        finishTempLine();
+                        tempString += "\n" + type;
+                        finishTempLine();
+                        tempString += "\n";
+                        finishTempLine();
+                        tempString += "\n";
+                        
+                        for(var key in code)
+                        {
+                            tempString += key + ',';
+                        }
+                        
+                        finishTempLine();
+                    }
+                    else
+                    {
+                        tempString = "";
+                    }
+                    
+                    tempString += "\n";
+
+                    for(var key in code)
+                    {
+                        tempString += code[key] + ',';
+                    }
+                    
+                    finishTempLine();
+                    
+                    return true;
+                }
+            }  
+            
+            return false;
+        };
+        
+        
+    $scope.exportCSV = function() {
+        
+        var stringAlcohol = "";
+        var stringExercise = "";
+        var stringJournal = "";
+        var stringNutrition = "";
+        var stringSleep = "";
+        var stringStepCount = "";
+        var stringWeight = "";
+        var stringWellbeing = "";
+        
+        var startedAlcohol = false;
+        var startedExercise = false;
+        var startedJournal = false;
+        var startedNutrition = false;
+        var startedSleep = false;
+        var startedStepCount = false;
+        var startedWeight = false;
+        var startedWellbeing = false;
+        
+        var totalString = "";//Settings:\n" + jsonString(localStorage.settings) 
+                       // + "\nProfile:\n"  + jsonString(localStorage.profile);
+            
+        //var json = JSON.parse(localStorage);
+        startedAlcohol = false;
+          
+          
+        for(var key in localStorage)
+        {
+            if(checkFor(key,"alcohol",startedAlcohol)) { startedAlcohol = true; stringAlcohol += tempString; }
+            else if(checkFor(key,"exercise",startedExercise)) { startedExercise = true; stringExercise += tempString; }
+            else if(checkFor(key,"journal",startedJournal)) { startedJournal = true; stringJournal += tempString; }
+            else if(checkFor(key,"nutrition",startedNutrition)) { startedNutrition = true; stringNutrition += tempString; }
+            else if(checkFor(key,"sleep",startedSleep)) { startedSleep = true; stringSleep += tempString; }
+            else if(checkFor(key,"stepcount",startedStepCount)) { startedStepCount = true; stringStepCount += tempString; }
+            else if(checkFor(key,"weight",startedWeight)) { startedWeight = true; stringWeight += tempString; }
+            else if(checkFor(key,"wellbeing",startedWellbeing)) { startedWellbeing = true; stringWellbeing += tempString; }
+        }
+           
+        tempString = "";
+        finishTempLine();
+        
+        totalString += tempString + stringAlcohol + stringExercise + stringJournal + stringNutrition + stringSleep + stringStepCount + stringWeight + stringWellbeing;   
+                
+        var encoding = encode(totalString);
+
+        cordova.plugins.email.open({
+             to:      '',
+             subject: 'My Ginsberg Data',
+             body:    'See attached Ginsberg Data',
+             attachments: 'base64:data.json//'+encoding
+        }); 
+    };
+
       
       $scope.exportData = function() {
           return [{a: 1, b:2}, {a:3, b:4}];
